fae0d3d33880941082d7f92a0b760d0bbef5ef82
Samuel Lee <sanelightspy@gmail.com>
2024-07-20 00:48:39 +0800

    Improved mobile responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="p-5 items-center bg-orange-100">
-      <div className="engrave flex gap-5">
+      <div className="engrave flex gap-5 text-3xl sm:text-4xl">
         <Link to="/">
-          <div className="pb-5 text-4xl">Home</div>
+          <div className="pb-5">Home</div>
         </Link>
         <Link to="/about">
-          <div className="pb-5 text-4xl">About</div>
+          <div className="pb-5">About</div>
         </Link>
         <Link to="/projects">
-          <div className="pb-5 text-4xl">Projects</div>
+          <div className="pb-5">Projects</div>
         </Link>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,10 @@ const Home = () => {
     <Section
       content={
         <div className="engrave flex flex-col gap-3 w-4/5 leading-none">
-          <div className="text-7xl sm:text-[100px] md:text-[140px] lg:text-[180px]">
+          <div className="text-6xl sm:text-[100px] md:text-[140px] lg:text-[180px]">
             Hello, I'm
           </div>
-          <div className="pb-20 text-7xl sm:text-[100px] md:text-[140px] lg:text-[180px] text-amber-900 text-nowrap">
+          <div className="pb-20 text-6xl sm:text-[100px] md:text-[140px] lg:text-[180px] text-amber-900 text-nowrap">
             Sambeige :)
           </div>
         </div>
