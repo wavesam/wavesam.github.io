f5509b5066fbc39e622b24b6e995749cc7140465
Samuel Lee <sanelightspy@gmail.com>
2024-07-23 00:24:13 +0800

    Engraved navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,18 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="p-5 items-center bg-orange-100 flex gap-5 text-3xl sm:text-4xl">
-      <Link to="/">
-        <div className="pb-5">Home</div>
-      </Link>
-      <Link to="/about">
-        <div className="pb-5">About</div>
-      </Link>
-      <Link to="/projects">
-        <div className="pb-5">Projects</div>
-      </Link>
+    <div className="p-5 items-center bg-orange-100 text-3xl sm:text-4xl">
+      <div className="engrave flex gap-5">
+        <Link to="/">
+          <div className="pb-5">Home</div>
+        </Link>
+        <Link to="/about">
+          <div className="pb-5">About</div>
+        </Link>
+        <Link to="/projects">
+          <div className="pb-5">Projects</div>
+        </Link>
+      </div>
     </div>
   );
 };
