371c66bb4be3b6116ab5727dea4b4ca941f8a238
Samuel Lee <sanelightspy@gmail.com>
2024-07-23 01:23:28 +0800

    Removed search bar on mobile

--- src/components/WebsiteFrame.jsx
+++ src/components/WebsiteFrame.jsx
@@ -1,15 +1,15 @@
 const WebsiteFrame = ({ site }) => {
   return (
     <div className="flex flex-col">
-      <div className="mt-5 w-full h-[45px] pt-[8px] bg-white rounded-t-3xl flex items-center justify-center">
-        <div className="w-[95%] md:w-1/2 h-full px-3 bg-gray-200 rounded-full text-center text-xl font-light flex justify-between items-center text-black">
+      <div className="hidden sm:flex mt-5 w-full h-[45px] pt-[8px] bg-white rounded-t-3xl flex items-center justify-center">
+        <div className="flex w-[95%] md:w-1/2 h-full px-3 bg-gray-200 rounded-full text-center text-xl font-light justify-between items-center text-black">
           <p className="absolute">🔎</p>
           <p></p>
           <div>{site}</div>
           <p></p>
         </div>
       </div>
-      <div className="w-full h-[700px] border-8 border-white rounded-b-3xl bg-white flex items-center justify-center">
+      <div className="w-full h-[700px] border-8 border-white rounded-3xl sm:rounded sm:rounded-b-3xl bg-white flex items-center justify-center">
         <iframe
           src={`https://${site}`}
           className="w-full h-full rounded-3xl bg-white"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,9 +15,7 @@ const Home = () => {
           <div className="text-3xl -mt-20">
             Check out some of my proudest works below.
           </div>
-          <h1 className="mt-20 text-6xl sm:text-[80px]">
-            1. Kwantum Institute
-          </h1>
+          <h1 className="mt-20 text-4xl">1. Kwantum Institute</h1>
           <p>
             I was the lead web developer at Kwantum Institute, responsible for
             the majority of the front-end development.
