eab1f134d25f6a374200b941f9c0a6025c56c9d7
Samuel Lee <sanelightspy@gmail.com>
2024-07-23 22:43:48 +0800

    Added about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,69 @@ const About = () => {
       content={
         <div className="w-4/5">
           <h1>About</h1>
-          <p>Hi, I'm Sambeige!</p>
+          <p>
+            <br />
+            At my core, I am a person who believes in the power of growth and
+            continuous learning. I have always approached everything in my life,
+            including academics, with a growth mindset - being a better person
+            than I was yesterday. Even though I will never be perfect, I still
+            strive to become the best I ever can be, and this translates deeply
+            into my attitude towards learning.
+            <br />
+            <br />
+            Although I have not readily decided on a career as an upcoming
+            undergraduate, I aspire to focus on the fields of computer science
+            and the internet, and it is highly likely that I will pursue web
+            development, a career that depends on one’s capability to learn new
+            things in a rapidly evolving landscape where new methods and
+            frameworks arise by the year. In my free time, I have dedicated
+            myself to building web applications, and my best work can be seen on
+            this website: https://sambeige.github.io. <br />
+            <br />
+            Besides computer science, I also like putting my hands in mechanical
+            engineering and introducing it to my undying passion to do better
+            for every build I make. A great example of this is when along with
+            two other friends, I took a trip to Macau to represent our school in
+            a robotics competition
+            (https://tis.edu.mo/news/gear-up-for-the-future). We finished as
+            finalists largely because our group spent countless hours
+            researching how other people built their robots online and applied
+            that knowledge to our own robot. Our robot, which started off as one
+            which produced average results, became a finalist because of me and
+            my friends’ dedication to improving the robot so that it can be the
+            best it can be. <br />
+            <br />
+            Another example of me adopting a growth mindset is in my academics
+            in high school. When I was in my freshman year, even though I was a
+            passionate learner, I was not getting the grades I desired and I
+            believed that I simply was not talented enough. But as I grew older,
+            I recognized that all it takes to achieve higher grades is a strong
+            dedication to improve, the knowledge of how to study and the
+            discipline to stay on task. Hence, my grades went from average to
+            above average in my junior year, and I achieved the high honors
+            award. I intend to continuously improve my methodology and mindset
+            for scoring higher in academics. <br />
+            <br />
+            Beyond my academic and technical pursuits, I also have a deep-rooted
+            passion for learning musical instruments and serving people with it.
+            In my free time, along with designing websites, I also play piano
+            improvisation and enjoy experimenting with new techniques.
+            Additionally, I can proficiently play the violin, the drums, and the
+            guitar, and I constantly challenge myself to get better at even more
+            instruments. Hence, if there is an opportunity for me to join an
+            orchestral club, I would gladly take it as I enjoy performing music
+            for an audience and also learning about how orchestras operate. I
+            also actively seek opportunities to use my music skills to serve my
+            society, with previous examples being joining the worship team and
+            the record club (a music concert club) at my high school and playing
+            piano for my youth church community once a month. <br />
+            <br />
+            With an attitude of growth and becoming a better version of myself
+            every day, I will apply the same mindset in my studies in
+            university. I will strive to score as high as I can academically and
+            be as capable as I can be socially, and also encourage my friends to
+            approach life with the same mindset.
+          </p>
         </div>
       }
     />
