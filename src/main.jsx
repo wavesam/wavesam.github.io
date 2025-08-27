52b781db2ad9194ae9df905a101b3a9d4ac4c772
samuel <samuel@samuels-MacBook-Air.local>
2025-08-27 22:14:50 +0800

    changing to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";  // Change this line
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>  {/* Change this line */}
       <App />
-    </BrowserRouter>
+    </HashRouter>  {/* Change this line */}
   </React.StrictMode>
 );
